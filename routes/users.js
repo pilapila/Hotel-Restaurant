d9971cabf32061f1bd031a36ecbb7c3bdf608a6c
pilapila <sina.shafigh@gmail.com>
2017-01-16 12:21:19 +0330

    Added Create user and form validation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../services/user-service');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/create', function(req, res, next) {
+	var vm = {
+		title: 'Create an account'
+	};
+  	res.render('users/create', vm);
+});
+
+router.post('/create', function(req, res, next) {
+	userService.addUser(req.body, function(err) {
+		if (err) {
+			console.log(err);
+			var vm = {
+			title: 'Create an account',
+			input: req.body,
+			error: err
+			};
+			delete vm.input.password;
+			return res.render('users/create', vm);
+		} // end if
+  		res.redirect('/orders');
+	}); // end userServices
+}); // end post
+
 
 module.exports = router;
